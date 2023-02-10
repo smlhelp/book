83e5e7c26e12fcaccf084214efbb364caa0e92a4
Thea Brick <thea@theabrick.com>
2023-02-10 16:29:25 -0500

    add searchbar (#74)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,6 +37,7 @@ const config = {
       }),
     ],
   ],
+  // themes: ['@docusaurus/theme-search-algolia'],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
@@ -54,6 +55,35 @@ const config = {
         theme,
         darkTheme,
         additionalLanguages: ["sml", "vim"],
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'XNI9A5Z2CN',
+
+        // Public API key: it is safe to commit it
+        apiKey: '5b35780ea0596402725411ca0bf319b0',
+
+        indexName: 'smlhelp',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+        //   from: '/docs/', // or as RegExp: /\/docs\//
+        //   to: '/',
+        // },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+
+        //... other Algolia params
       },
     }),
   // TODO figure out how to get the katex css without CDN (should already be
